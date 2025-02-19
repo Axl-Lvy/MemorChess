fb157ad1f8e1173c4ca450a2252a98ce4f403c78
Axl <axeldecesson@live.fr>
2025-02-20 00:26:21 +0100

    Add kotlinx-datetime library and display today's date
    Added the kotlinx-datetime library to dependencies for date handling. Updated the UI to display the current date using the new library in the App component. Refactored code slightly to accommodate the new functionality.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -60,6 +60,7 @@ kotlin {
       implementation(compose.components.uiToolingPreview)
       implementation(libs.androidx.lifecycle.viewmodel)
       implementation(libs.androidx.lifecycle.runtime.compose)
+      implementation(libs.kotlinx.datetime)
     }
     commonTest.dependencies { implementation(libs.kotlin.test) }
   }
