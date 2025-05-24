5869e6b0656a34f4cad694dc0247fad8396bf0cb
Axl <axeldecesson@live.fr>
2025-05-24 12:20:00 +0200

    Remove git hooks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,6 @@ pluginManagement {
 }
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.21"
 }
 
 dependencyResolutionManagement {
@@ -33,15 +32,3 @@ dependencyResolutionManagement {
 }
 
 include(":composeApp")
-
-gitHooks {
-    commitMsg {
-        conventionalCommits()
-    }
-
-    preCommit {
-        tasks("ktfmtCheck","desktopTest", "connectedAndroidTest")
-    }
-
-    createHooks(true)
-}
