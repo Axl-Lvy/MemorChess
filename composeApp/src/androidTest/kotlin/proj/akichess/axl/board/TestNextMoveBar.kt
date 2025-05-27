44c976501d76dc0534b3b14586fdf89633ade415
Axl-Lvy <axl@activeviam.com>
2025-05-27 20:31:26 +0200

    test: add next moves tests

---
+++ composeApp/src/androidTest/kotlin/proj/akichess/axl/board/TestNextMoveBar.kt
@@ -0,0 +1,70 @@
+package proj.akichess.axl.board
+
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertContentDescriptionContains
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import org.junit.Rule
+import proj.akichess.axl.utils.hasClickLabel
+import proj.ankichess.axl.core.intf.data.DatabaseHolder
+import proj.ankichess.axl.test_util.TestDataBase
+import proj.ankichess.axl.test_util.getNextMoveDescription
+import proj.ankichess.axl.test_util.getTileDescription
+import proj.ankichess.axl.ui.components.control.board_control.ControllableBoardPage
+
+class TestNextMoveBar {
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @BeforeTest
+  fun setUp() {
+    DatabaseHolder.init(TestDataBase.vienna())
+    composeTestRule.setContent { ControllableBoardPage() }
+    composeTestRule.onNode(hasClickLabel(getTileDescription("e2"))).assertExists().performClick()
+    composeTestRule.onNode(hasClickLabel(getTileDescription("e4"))).assertExists().performClick()
+  }
+
+  @Test
+  fun testNextMoveAppears() {
+    composeTestRule.onNodeWithTag("Back").assertExists().performClick()
+    composeTestRule.onNodeWithTag(getNextMoveDescription("e4")).assertExists()
+  }
+
+  @Test
+  fun testNextMoveWorks() {
+    composeTestRule.onNodeWithTag("Back").assertExists().performClick()
+    composeTestRule.onNodeWithTag(getNextMoveDescription("e4")).assertExists().performClick()
+    composeTestRule
+      .onNode(hasClickLabel(getTileDescription("e4")))
+      .assertExists()
+      .assertContentDescriptionContains("Piece P")
+    composeTestRule
+      .onNode(hasClickLabel(getTileDescription("e2")))
+      .assert(hasContentDescription("", substring = true).not())
+  }
+
+  @Test
+  fun testMultipleNextMoves() {
+    composeTestRule.onNodeWithTag("Back").assertExists().performClick()
+    composeTestRule.onNode(hasClickLabel(getTileDescription("e2"))).assertExists().performClick()
+    composeTestRule.onNode(hasClickLabel(getTileDescription("e3"))).assertExists().performClick()
+    composeTestRule.onNodeWithTag("Back").assertExists().performClick()
+
+    composeTestRule.onNodeWithTag(getNextMoveDescription("e4")).assertExists()
+    composeTestRule.onNodeWithTag(getNextMoveDescription("e3")).assertExists().performClick()
+    composeTestRule
+      .onNode(hasClickLabel(getTileDescription("e3")))
+      .assertExists()
+      .assertContentDescriptionContains("Piece P")
+    composeTestRule
+      .onNode(hasClickLabel(getTileDescription("e2")))
+      .assert(hasContentDescription("", substring = true).not())
+    composeTestRule
+      .onNode(hasClickLabel(getTileDescription("e4")))
+      .assert(hasContentDescription("", substring = true).not())
+  }
+}
