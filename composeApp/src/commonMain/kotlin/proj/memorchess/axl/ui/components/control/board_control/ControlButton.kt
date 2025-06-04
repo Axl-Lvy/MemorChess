355659aa1d46e63fb31d7134e78249cab7308368
Axl-Lvy <axl@activeviam.com>
2025-06-04 13:44:04 +0200

    format

--- composeApp/src/commonMain/kotlin/proj/memorchess/axl/ui/components/control/board_control/ControlButton.kt
+++ composeApp/src/commonMain/kotlin/proj/memorchess/axl/ui/components/control/board_control/ControlButton.kt
@@ -7,16 +7,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
-import memorchess.composeapp.generated.resources.Res
-import memorchess.composeapp.generated.resources.description_board_back
-import memorchess.composeapp.generated.resources.description_board_next
-import memorchess.composeapp.generated.resources.description_board_reset
-import memorchess.composeapp.generated.resources.description_board_reverse
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.ArrowLeft
 import compose.icons.feathericons.ArrowRight
 import compose.icons.feathericons.Repeat
 import compose.icons.feathericons.Rewind
+import memorchess.composeapp.generated.resources.Res
+import memorchess.composeapp.generated.resources.description_board_back
+import memorchess.composeapp.generated.resources.description_board_next
+import memorchess.composeapp.generated.resources.description_board_reset
+import memorchess.composeapp.generated.resources.description_board_reverse
 import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.stringResource
 
