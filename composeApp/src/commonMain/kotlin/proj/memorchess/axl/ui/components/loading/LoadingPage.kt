c1a2f63650842d6ff1f782a898ad02ec5e972035
Axl-Lvy <axl@activeviam.com>
2025-06-06 12:19:30 +0200

    perf: ensure minimum loading time for better UX

--- composeApp/src/commonMain/kotlin/proj/memorchess/axl/ui/components/loading/LoadingPage.kt
+++ composeApp/src/commonMain/kotlin/proj/memorchess/axl/ui/components/loading/LoadingPage.kt
@@ -11,13 +11,29 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.measureTime
 
+/**
+ * A composable that displays a loading indicator while a suspending function is executed. Once the
+ * function completes, it displays the provided composable content.
+ *
+ * This composable ensures that the loading indicator is shown for at least
+ * [a minimum duration][MINIMUM_LOADING_TIME]
+ *
+ * @param suspendingFunction The suspending function to execute.
+ * @param composable The composable content to display after loading.
+ */
 @Composable
 fun LoadingPage(suspendingFunction: suspend () -> Any?, composable: @Composable () -> Unit) {
+
   var isLoading by remember { mutableStateOf(true) }
 
   LaunchedEffect(Unit) {
-    suspendingFunction()
+    val timeTaken = measureTime { suspendingFunction() }
+    if (timeTaken < MINIMUM_LOADING_TIME) {
+      kotlinx.coroutines.delay(MINIMUM_LOADING_TIME - timeTaken)
+    }
     isLoading = false
   }
 
@@ -29,3 +45,9 @@ fun LoadingPage(suspendingFunction: suspend () -> Any?, composable: @Composable 
     composable()
   }
 }
+
+/**
+ * The minimum time the loading indicator should be displayed, even if the suspending function
+ * completes faster.
+ */
+private val MINIMUM_LOADING_TIME = 1.seconds
