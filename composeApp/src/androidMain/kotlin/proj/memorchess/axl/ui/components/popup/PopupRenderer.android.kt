21f70ccfc1e023aca8ce07f74bec5b6266cccc00
Axel Levy <81815421+Axl-Lvy@users.noreply.github.com>
2025-07-06 00:19:17 +0200

    fix: ensure toast messages are displayed on the main thread (#36)

--- composeApp/src/androidMain/kotlin/proj/memorchess/axl/ui/components/popup/PopupRenderer.android.kt
+++ composeApp/src/androidMain/kotlin/proj/memorchess/axl/ui/components/popup/PopupRenderer.android.kt
@@ -3,10 +3,13 @@ package proj.memorchess.axl.ui.components.popup
 import android.widget.Toast
 import proj.memorchess.axl.MAIN_ACTIVITY
 
-private var currentToast: Toast = Toast.makeText(MAIN_ACTIVITY, "", Toast.LENGTH_SHORT)
+private var currentToast: Toast? = null
 
 actual fun getToastRenderer() = IToastRenderer { message, _ ->
-  currentToast.cancel()
-  currentToast = Toast.makeText(MAIN_ACTIVITY, message, Toast.LENGTH_SHORT)
-  currentToast.show()
+  MAIN_ACTIVITY.runOnUiThread {
+    currentToast?.cancel()
+    val newToast = Toast.makeText(MAIN_ACTIVITY, message, Toast.LENGTH_SHORT)
+    newToast.show()
+    currentToast = newToast
+  }
 }
