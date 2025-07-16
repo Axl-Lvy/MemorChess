b6e230b5f169bd894093ff3851a88a1c35748749
Axl-Lvy <axl@activeviam.com>
2025-07-16 13:23:02 +0200

    nosonar on both lines

--- composeApp/src/commonMain/kotlin/proj/memorchess/axl/core/data/online/Supabase.kt
+++ composeApp/src/commonMain/kotlin/proj/memorchess/axl/core/data/online/Supabase.kt
@@ -6,7 +6,7 @@ import io.github.jan.supabase.postgrest.Postgrest
 
 // This is a Supabase anon (public) key. It is safe to expose in client-side code.
 // See: https://supabase.com/docs/guides/auth#api-keys
-private const val apiKey =
+private const val apiKey = // NOSONAR
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhxdm5lZ2FrcWNnbHRtcnp2b3hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyNDQzOTUsImV4cCI6MjA2NzgyMDM5NX0.IZreLXFrOzxdwyg2t3ckovtD_Izt4Hn-bAKbGcFjj2A" // NOSONAR
 
 private const val url = "https://hqvnegakqcgltmrzvoxi.supabase.co"
