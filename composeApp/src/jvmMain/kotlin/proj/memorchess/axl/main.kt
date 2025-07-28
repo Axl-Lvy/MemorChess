86d7773605f524191bcd0b85c6fa18618ccf1aae
Axl <axeldecesson@live.fr>
2025-07-29 00:00:42 +0200

    ci: simplify main function signature

--- composeApp/src/jvmMain/kotlin/proj/memorchess/axl/main.kt
+++ composeApp/src/jvmMain/kotlin/proj/memorchess/axl/main.kt
@@ -3,7 +3,7 @@ package proj.memorchess.axl
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main(args: Array<String>) = application {
+fun main() = application {
   Window(onCloseRequest = ::exitApplication, alwaysOnTop = true, title = "Memor Chess") {
     KoinStarterApp()
   }
