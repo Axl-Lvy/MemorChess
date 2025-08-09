14aa4a95afc705dd7a3e61eeb0c3d9478fe1395e
Axel Levy <81815421+Axl-Lvy@users.noreply.github.com>
2025-08-09 15:51:06 +0200

    refactor: cleaning (#63)

--- composeApp/src/commonMain/kotlin/proj/memorchess/axl/core/data/online/database/SupabaseDataUtil.kt
+++ composeApp/src/commonMain/kotlin/proj/memorchess/axl/core/data/online/database/SupabaseDataUtil.kt
@@ -21,8 +21,6 @@ internal const val ORIGIN_FIELD = "origin"
 internal const val DESTINATION_FIELD = "destination"
 internal const val NAME_FIELD = "name"
 internal const val MOVE_ID_FIELD = "move_id"
-internal const val IS_GOOD_FIELD = "is_good"
-internal const val CREATED_AT_FIELD = "created_at"
 internal const val USER_ID_FIELD = "user_id"
 internal const val IS_DELETED_FIELD = "is_deleted"
 internal const val UPDATED_AT_FIELD = "updated_at"
@@ -54,52 +52,11 @@ internal data class RemotePosition(
 )
 
 @Serializable
-internal data class RemotePositionToUpload(
-  @SerialName(FEN_REPRESENTATION_FIELD) val fenRepresentation: String
-)
-
-@Serializable
 internal data class RemoteMove(
   @SerialName(ID_FIELD) val id: Long,
   @SerialName(ORIGIN_FIELD) val origin: Long,
   @SerialName(DESTINATION_FIELD) val destination: Long,
   @SerialName(NAME_FIELD) val name: String,
-)
-
-@Serializable
-internal data class RemoteUserMove(
-  @SerialName(ID_FIELD) val id: Long,
-  @SerialName(MOVE_ID_FIELD) val moveId: Long,
-  @SerialName(IS_GOOD_FIELD) val isGood: Boolean,
-  @SerialName(CREATED_AT_FIELD) val createdAt: LocalDateTime,
-  @SerialName(USER_ID_FIELD) val userId: String,
-  @SerialName(IS_DELETED_FIELD) val isDeleted: Boolean,
-  @SerialName(UPDATED_AT_FIELD) val updatedAt: LocalDateTime,
-)
-
-@Serializable
-internal data class RemoteUserPosition(
-  @SerialName(ID_FIELD) val id: Long,
-  @SerialName(USER_ID_FIELD) val userId: String,
-  @SerialName(POSITION_ID_FIELD) val positionId: Long,
-  @SerialName(DEPTH_FIELD) val depth: Int,
-  @SerialName(NEXT_TRAINING_DATE_FIELD) val nextTrainingDate: LocalDate,
-  @SerialName(LAST_TRAINING_DATE_FIELD) val lastTrainingDate: LocalDate,
-  @SerialName(CREATED_AT_FIELD) val createdAt: LocalDateTime,
-  @SerialName(IS_DELETED_FIELD) val isDeleted: Boolean,
-  @SerialName(UPDATED_AT_FIELD) val updatedAt: LocalDateTime,
-)
-
-@Serializable
-internal data class RemoteUserPositionToUpload(
-  @SerialName(USER_ID_FIELD) val userId: String,
-  @SerialName(POSITION_ID_FIELD) val positionId: Long,
-  @SerialName(DEPTH_FIELD) val depth: Int,
-  @SerialName(NEXT_TRAINING_DATE_FIELD) val nextTrainingDate: LocalDate,
-  @SerialName(LAST_TRAINING_DATE_FIELD) val lastTrainingDate: LocalDate,
-  @SerialName(CREATED_AT_FIELD) val createdAt: LocalDateTime,
-  @SerialName(IS_DELETED_FIELD) val isDeleted: Boolean,
-  @SerialName(UPDATED_AT_FIELD) val updatedAt: LocalDateTime,
 )
 
 @Serializable
