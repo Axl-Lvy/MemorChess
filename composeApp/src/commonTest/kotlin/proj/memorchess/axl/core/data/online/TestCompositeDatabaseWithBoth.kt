ddfbcd801d7bd8dc4ea0ea707b981d6ae766c561
Axl <axeldecesson@live.fr>
2025-12-10 19:49:39 +0100

    remove unused test

--- composeApp/src/commonTest/kotlin/proj/memorchess/axl/core/data/online/TestCompositeDatabaseWithBoth.kt
+++ composeApp/src/commonTest/kotlin/proj/memorchess/axl/core/data/online/TestCompositeDatabaseWithBoth.kt
@@ -1,18 +1,13 @@
 package proj.memorchess.axl.core.data.online
 
-import kotlin.test.Ignore
 import kotlin.test.Test
-import kotlin.test.assertFailsWith
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 import kotlinx.coroutines.test.runTest
-import kotlinx.datetime.LocalTime
-import kotlinx.datetime.atTime
-import kotlinx.datetime.toInstant
 import org.koin.core.component.inject
 import proj.memorchess.axl.core.data.DataNode
 import proj.memorchess.axl.core.data.online.database.DatabaseSynchronizer
 import proj.memorchess.axl.core.date.DateUtil
-import proj.memorchess.axl.core.date.DateUtil.toInstant
 import proj.memorchess.axl.core.date.PreviousAndNextDate
 import proj.memorchess.axl.core.engine.Game
 import proj.memorchess.axl.core.graph.nodes.PreviousAndNextMoves
@@ -33,26 +28,20 @@ class TestCompositeDatabaseWithBoth : TestCompositeDatabase.TestCompositeDatabas
     assertTrue { compositeDatabase.isActive() }
   }
 
-  @Ignore
   @Test
-  fun testLastUpdatedFail() = runTest {
+  fun testConsistency() = runTest {
     val game = Game()
-    val node1 =
+    val node =
       DataNode(
         game.position.createIdentifier(),
         PreviousAndNextMoves(),
         PreviousAndNextDate.dummyToday(),
-        DateUtil.tomorrow().atTime(LocalTime(0, 0, 0)).toInstant(),
       )
-    localDatabase.insertNodes(node1)
-    val node2 =
-      DataNode(
-        game.position.createIdentifier(),
-        PreviousAndNextMoves(),
-        PreviousAndNextDate.dummyToday(),
-        DateUtil.now(),
-      )
-    remoteDatabase.insertNodes(node2)
-    assertFailsWith<IllegalStateException> { compositeDatabase.getLastUpdate() }
+
+    remoteDatabase.insertNodes(node)
+    assertEquals(
+      remoteDatabase.getPosition(node.positionIdentifier),
+      remoteDatabase.getPosition(node.positionIdentifier),
+    )
   }
 }
