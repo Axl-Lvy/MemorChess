3036afac69a4c99ed7a69e99fd091124c302876a
Axl-Lvy <axl@activeviam.com>
2026-03-07 23:24:08 +0100

    java 21

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -27,7 +27,7 @@ kotlin {
   androidTarget {
     @OptIn(ExperimentalKotlinGradlePluginApi::class)
     instrumentedTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
-    compilerOptions { jvmTarget.set(JvmTarget.JVM_17) }
+    compilerOptions { jvmTarget.set(JvmTarget.JVM_21) }
   }
 
   // JVM/Desktop configuration
@@ -194,8 +194,8 @@ android {
   }
 
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
   }
 
   testOptions {
