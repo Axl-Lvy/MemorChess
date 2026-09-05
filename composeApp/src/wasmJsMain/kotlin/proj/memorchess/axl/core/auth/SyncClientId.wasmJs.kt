f99eab8b386956b8de85a6cf1fc9a85d6e261de0
Axl-Lvy <axl@activeviam.com>
2026-09-05 18:57:44 +0200

    feat(composeApp): point wasmJs's sync redirect URI at /sync-oauth-callback

--- composeApp/src/wasmJsMain/kotlin/proj/memorchess/axl/core/auth/SyncClientId.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/proj/memorchess/axl/core/auth/SyncClientId.wasmJs.kt
@@ -5,4 +5,4 @@ import kotlinx.browser.window
 actual val SYNC_CLIENT_ID: String = "9o2yn87g7bwvpewsqijxe"
 
 actual val SYNC_REDIRECT_URI: String
-  get() = "${window.location.origin}/oauth-callback"
+  get() = "${window.location.origin}$SYNC_REDIRECT_PATH"
